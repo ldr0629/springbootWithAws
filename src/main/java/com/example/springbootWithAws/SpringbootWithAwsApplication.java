b024c3bc0919fec57e6855f29eb091dde81d8404
Administrator <leedero01@naver.com>
2022-12-20 02:54:50 +0900

    Jpa Auditing 활성화

---
+++ src/main/java/com/example/springbootWithAws/SpringbootWithAwsApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootWithAws;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class SpringbootWithAwsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithAwsApplication.class, args);
+	}
+}
