7a3b2f2996a7654d272245b6d37bfdd54608be4a
Administrator <leedero01@naver.com>
2022-12-21 03:47:30 +0900

    IndexController API 수정

--- src/main/java/com/example/springbootWithAws/web/IndexController.java
+++ src/main/java/com/example/springbootWithAws/web/IndexController.java
@@ -1,12 +1,22 @@
 package com.example.springbootWithAws.web;
 
+import com.example.springbootWithAws.service.PostsService;
+import com.example.springbootWithAws.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
+
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
@@ -14,4 +24,12 @@ public class IndexController {
     public String postsSave() {
         return "posts-save";
     }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
+    }
+
 }
