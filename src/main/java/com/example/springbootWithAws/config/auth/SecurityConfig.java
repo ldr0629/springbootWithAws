8d5fd8cb39a1a0b06d4aafd5fde1f7c604600b04
Administrator <leedero01@naver.com>
2022-12-25 05:41:10 +0900

    SecurityConfig 설정

---
+++ src/main/java/com/example/springbootWithAws/config/auth/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.example.springbootWithAws.config.auth;
+
+import com.example.springbootWithAws.domain.user.Role;
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/", "/css/**", "/images/**",
+                        "/js/**", "/h2-console").permitAll()
+                .requestMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+        return http.build();
+    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .headers().frameOptions().disable()
+//                .and()
+//                .authorizeHttpRequests()
+//                .requestMatchers("/", "/css/**", "/images/**",
+//                        "/js/**", "/h2-console").permitAll()
+//                .requestMatchers("/api/v1/**").hasRole(Role.USER.name())
+//                .anyRequest().authenticated()
+//                .and()
+//                .logout()
+//                .logoutSuccessUrl("/")
+//                .and()
+//                .oauth2Login()
+//                .userInfoEndpoint()
+//                .userService(customOAuth2UserService);
+//    }
+
+}
